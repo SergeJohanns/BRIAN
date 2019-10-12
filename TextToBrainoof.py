4d7832d47ddcf54ba88f2abdb5c0399829b304a4
SergeJohanns <serge.johanns@gmail.com>
2019-10-13 00:55:14 +0200

    Implemented multipass

--- TextToBrainoof.py
+++ TextToBrainoof.py
@@ -2,42 +2,55 @@
 #It is designed to optimise the code for shortness, but the code isn't as optimal as a human could make it and it comes with some overhead.
 #TODO: Swap block method for multipass method
 
+import math
+
 def TextToBF(string): #The input function
     return IntListToBF(list(map(ord, string)))
 
 def IntListToBF(intListIn): #The main processing function
-    intList = ToBlocks(intListIn) #Break the input list up into blocks
-    differences = [[list(map(lambda n: n - x, block)) for x in range(min(block), max(block) + 1)] for block in intList] #Get a list of the differences to all possible base integers (what the counter will count to) for all blocks
-    distances = [[sum(map(abs, diff)) for diff in block] for block in differences] #Sum the differences into the total distance, which is a measure for the amount of +s and -s that will have to be added.
-    for i in range(len(distances)): #Weigh the distances for loop overhead
-        base = min(intList[i])
-        for j in range(len(distances[i])):
-            distances[i][j] += sum(GetDivisors(base + j)) + 5 * len(GetDivisors(base + j)) #The sum of all prime factors, which is a measure for +s and -s, plus 5 times the amount of factors, which is the amount of characters needed for loops (one loop comes with 5 characters '[><-]' of overhead)
-    ns = [block.index(min(block)) for block in distances] #Find the optimal base integers for each block
-    output = "" #Prepare the output
-    for i in range(len(distances)): #For every block
-        output += BFGenerator(differences[i][ns[i]], ns[i] + min(intList[i])) #Add the fully functional code for that block to the output
-    return output
+    n = FindBaseInteger(intListIn)
+    coeffList = [[(c+1) + abs(m - ((c+1) * n)) for c in range(math.ceil(m / n))] for m in intListIn]
+    coeffList = [coeffs.index(min(coeffs)) + 1 for coeffs in coeffList]
+    diffList  = [intListIn[i] - n * coeffList[i] for i in range(len(intListIn))]
+    return BFGenerator(diffList, n, coeffList)
 
-def ToBlocks(intList): #Breaks the integer list into blocks where groups are too far apart in value
-    blocks = []
-    base = 0
-    for i in range(1, len(intList)): #For every element
-        divisors = GetDivisors(intList[i]) #Get its divisors
-        if sum(divisors) + 5 * len(divisors) < abs(intList[i] - (sum(intList[base:i]) // (i - base))): #If the overhead for making a new block is less than the cost of keeping it in the current block
-            blocks.append(intList[base:i]) #Store the old block
-            base = i #Set the new block
-    blocks.append(intList[base:])
-    return blocks
+def FindBaseInteger(intListIn):
+    A = [[x for x in range(0, max(intListIn) - i, i + 1)] for i in range(max(intListIn))] #Get a two-dimensional list [[1,2..k],[2,4..k]..[k]]
+    B = []
+    i = 0 #Counter for n
+    for n in intListIn:
+        B.append([]) #Add a two-dimenstional list of the form of A
+        j = 0 #Counter for col
+        for col in A:
+            B[i].append([]) #Add a column to that list
+            k = 1 #Counter for elem 
+            for elem in col:
+                B[i][j].append(k+abs(n-elem))
+                k += 1
+            j += 1
+        i += 1
+    B = [[min(col) for col in matrix] for matrix in B] #Select the most efficient coefficient for each letter for each basis integer
+    B = [sum([B[j][i] for j in range(len(B))]) for i in range(len(B[0]))] #Sum the costs of every letter assuming the most efficient coefficient for each basis integer
+    B = [B[i] + sum(GetDivisors(i + 1)) + 5 * len(GetDivisors(i + 1)) for i in range(len(B))] #Correct the weights for the length of the integer counter construct
+    return B.index(min(B)) + 1 #Return the most efficient basis integer
 
-def BFGenerator(diffList, base): #Generates the actual code
+def BFGenerator(diffList, base, coefficients): #Generates the actual code
+    print(diffList, base, coefficients)
+    #Prepare the basis integer
     if len(diffList) == 0:
         return ""
     divisors = GetDivisors(base)
-    output = "+" + ">+" * (len(diffList)-1) + "<" * (len(diffList) - 1)
+    output = ""
+    for i in coefficients:
+        output += "+" * i + ">"
+    output = output[:-1] + "[<]" #Cut off the final move command and then move back
+
+    output = ">" + output
+    
     for i in divisors:
         output = "+" * i + "[>" + output + "<-]" #Add another layer to the counters
-    output += ">" * (len(divisors) - 1)
+    #Add the adjustments
+    output += ">" * len(divisors) #Move to the cell after the last counter (the marking empty cell) and one more
     for i in diffList:
         if i >= 0:
             output += ">" + "+" * i
@@ -67,3 +80,11 @@ def Reduce(intList): #Reduces a list of 2s and 3s to minimal (as low as possible
     if len(intList) % 2 != 0:
         out.append(intList[len(intList) - 1])
     return out
+
+if __name__ == "__main__":
+    while True:
+        string = input("Text: ")
+        if string: #If string is not empty
+            print(TextToBF(string))
+        else:
+            break
